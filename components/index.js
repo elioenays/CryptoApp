a02ec5607c28c7df5d9b0d9674bde380a9d830d8
Elioenay <elionay407@gmail.com>
2022-04-03 20:00:49 -0300

    first commit

---
+++ components/index.js
@@ -0,0 +1,6 @@
+import { CircleButton } from './Button';
+import FocusedStatusBar from './FocusedStatusBar';
+import HomeHeader from './HomeHeader';
+import NFTCard from './NFTCard';
+
+export { HomeHeader, FocusedStatusBar, NFTCard, CircleButton };
