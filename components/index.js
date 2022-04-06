4bd85dd03fa57c4573422a1e2988e71dfa086a6f
Elioenay <elionay407@gmail.com>
2022-04-05 23:21:24 -0300

    Details Screen

--- components/index.js
+++ components/index.js
@@ -1,6 +1,18 @@
-import { CircleButton } from './Button';
+import { CircleButton, RectButton } from './Button';
+import DetailsBid from './DetailsBid';
+import DetailsDesc from './DetailsDesc';
 import FocusedStatusBar from './FocusedStatusBar';
 import HomeHeader from './HomeHeader';
 import NFTCard from './NFTCard';
+import { SubInfo } from './SubInfo';
 
-export { HomeHeader, FocusedStatusBar, NFTCard, CircleButton };
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  DetailsBid,
+  DetailsDesc,
+  RectButton,
+  SubInfo,
+};
